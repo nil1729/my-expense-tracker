5d983774019e20b55dacbd7fbb472c2f5f27b608
NILANJAN DEB <nilanjan172nsvian@gmail.com>
2024-03-31 11:25:07 +0530

    add default last-key-ts value on cache

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const logger = require('./config/logger');
-const { setupScheduler } = require('./scheduler');
-const { startWorker } = require('./worker');
-const { writeCredentialsToFile } = require('./google-auth/config');
+const logger = require("./config/logger");
+const { setupScheduler, setExpenseLastTsKey } = require("./scheduler");
+const { startWorker } = require("./worker");
+const { writeCredentialsToFile } = require("./google-auth/config");
 
 app.listen(PORT, async function () {
-  logger.info('environment: ' + process.env.NODE_ENV);
-  if (process.env.NODE_ENV === 'production') {
+  logger.info("environment: " + process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
     writeCredentialsToFile();
     await setupScheduler();
+    await setExpenseLastTsKey();
     startWorker();
   }
   logger.info(`server listening on port ${PORT}`);
